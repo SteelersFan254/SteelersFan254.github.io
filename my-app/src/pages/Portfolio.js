e6371bb12779cb922430cba3a477c6e551227d53
Ryan Borja <rcborja.school@gmail.com>
2020-03-10 07:42:33 -0500

    add info about Fanquiry

--- my-app/src/pages/Portfolio.js
+++ my-app/src/pages/Portfolio.js
@@ -13,10 +13,28 @@ function Portfolio() {
                     </a>
                     <div className="projectDetails">
                         <div className="descriptionBox">
-                            <p>Desciption</p>
+                            <h3>Desciption</h3>
+                            <hr />
+                            <p> Fanquiry was my very first group project in coding bootcamp. The main goal for the project was to make a website using AJAX calls to at least two different APIs. This site uses the lastfm API to get artist info based on a search as well as their album info. You can than click to see upcoming shows using the ticketmaster API. This was a 3-man group project. The others were in charge of html and css while I was in charge of all the JavaScript and APIs.
+                            </p>
+
                         </div>
                         <div className="skillsBox">
-                            <p>Skills</p>
+                            <h3>Learning</h3>
+                            <hr />
+                            <h4>New Skills Developed</h4>
+                            <ul>
+                                <li>Ajax calls</li>
+                                <li>Dynamically created html and css with jQuery</li>
+                                <li>GitHub - pushing and pulling in a group repo</li>
+                            </ul>
+                            <h4>Skills Improved</h4>
+                            <ul>
+                                <li>General JavaScript</li>
+                                <li>OnClick events and input submitting</li>
+                                <li>Working as a group - dividing up tasks based each group member's skills and abilities</li>
+                                <li>Time management - Splitting the days into different goals set up as checkpoints to have the project finished in time</li>
+                            </ul>
                         </div>
                     </div>
                 </div>
